c12961d6753e0bc041242c2535f679726ac4f4fe
mozyes <80746075+mozyes@users.noreply.github.com>
2023-07-11 11:16:54 +0545

    Create day1_BandNameGenerator.py

---
+++ Project/day1_BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator\n")
+first_name = input("Which flower do you love the most?\n")
+second_name = input("Which city do you grew up in?\n")
+print("Your band name could be " + first_name + " " + second_name)
