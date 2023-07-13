938cba04567bc2ac7d78222051693834656a6fee
mozyes <80746075+mozyes@users.noreply.github.com>
2023-07-13 11:21:30 +0545

    Create day3_treasurehuntgame.py

---
+++ Project/day3_treasurehuntgame.py
@@ -0,0 +1,45 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+decision_num1 = input("Do you want to take left path or right path (left/right)? ")
+if decision_num1.lower() == "left":
+    print("You are in front of a lake. ")
+    decision_num2 = input("do you want to swim across the lake or wait for a boat(swim/wait)? ")
+    if decision_num2.lower() == "wait":
+        print("you have reached a hall with 3 doors with 3 colors red, blue and yellow.")
+        decision_num3 = input("Which door would you choose(red, blue or yellow)? ")
+        if decision_num3.lower() == "yellow":
+            print("You Win!!!")
+        elif decision_num3.lower() == "red":
+            print("You lose!! you were burned by fire.")
+        else:
+            print("You lose!! You were eaten by beasts.")
+    else:
+        print("You lose!! You were attacked by trout.")
+else:
+    print("You lose!! You fell into a hole.")
