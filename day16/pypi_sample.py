ea138195077ef1148c37c097ddadcaebf4eaaa78
mozyes <80746075+mozyes@users.noreply.github.com>
2023-07-26 11:17:43 +0545

    Create pypi_sample.py

---
+++ day16/pypi_sample.py
@@ -0,0 +1,14 @@
+# firstly installed prettytable from pypi using terminal. Then, according to the format given by the aurthor of the package. Using it in following.
+
+import prettytable
+
+table = prettytable.PrettyTable()
+table.field_names = ["Pokeman name", "Type"]
+table.add_rows(
+    [
+        ["Pikachu", "Electric"],
+        ["Squirtle", "Water"],
+        ["Charmander", "Fire"],
+    ]
+)
+print(table)
