a06ff88ad266de51e2ee6d72f623289776d4e963
mozyes <80746075+mozyes@users.noreply.github.com>
2023-07-26 10:39:45 +0545

    Create oop_practice.py

---
+++ day16/oop_practice.py
@@ -0,0 +1,13 @@
+# practicing OOP!
+# simply put it. suppose in restaurant, waiter would be class, waiter a,b,c would be object. what waiter can do is method. and what are waiter's skill and stuff is attributes. Also, capitalize first letter in Class.
+
+import turtle
+
+timmy = turtle.Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("red")
+timmy.forward(100)
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
