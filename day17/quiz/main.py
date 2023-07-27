f63ca39d62affd044a57649f75a42aa0a311aea4
mozyes <80746075+mozyes@users.noreply.github.com>
2023-07-27 11:39:53 +0545

    Create main.py

---
+++ day17/quiz/main.py
@@ -0,0 +1,18 @@
+import data
+import question_model
+import quiz_brain
+
+question_bank = []
+for question in data.question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = question_model.Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = quiz_brain.QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("you've completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
