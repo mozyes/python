e46108b4d481ec9dfe1a84e6ba6227bd0aeb47bd
mozyes <80746075+mozyes@users.noreply.github.com>
2023-07-28 10:49:51 +0545

    Create sqaure_turtle_graphics.py

---
+++ day18/sqaure_turtle_graphics.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+
+for i in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.left(90)
+
+
+screen = Screen()
+screen.exitonclick()
