3ebf0f5bd2a12c5a5ae37a0141d9fb6b1489e044
mozyes <80746075+mozyes@users.noreply.github.com>
2023-07-31 09:58:37 +0545

    Update inhertitance_practice.py

--- day21/inhertitance_practice.py
+++ day21/inhertitance_practice.py
@@ -9,10 +9,13 @@ class Fish(Animal):
     def __init__(self):
         super().__init__()
     
+    def breathe(self):
+        super().breathe()
+        print("doing this underwater.")
+    
     def swim(self):
         print("moving in water.")
-
-
+        
 nemo = Fish()
 nemo.swim()
 nemo.breathe()
