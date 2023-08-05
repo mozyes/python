4c690c8effa4fb769708a0f1d881cd5d648112c2
mozyes <80746075+mozyes@users.noreply.github.com>
2023-08-05 10:08:44 +0545

    Create list_comprehension_practice.py

---
+++ day26/list_comprehension_practice.py
@@ -0,0 +1,11 @@
+# numbers = [1, 2, 3]
+# new_list = []
+# for n in numbers:
+#     add_1 = n+1
+#     new_list.append(add_1)
+# print(new_list)
+
+# the lines below are same code as above!
+numbers = [1, 2, 3]
+new_list = [n + 1 for n in numbers]
+print(new_list)
